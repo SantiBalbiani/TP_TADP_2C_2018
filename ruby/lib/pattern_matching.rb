5118d1054197f59a81b89aebb7c470ab91503b42
SantiBalbiani <santiagobalbiani@yahoo.com>
2018-11-25 21:59:46 -0300

    Matchers Basicos OK. Sigo con DuckTyping

--- ruby/lib/pattern_matching.rb
+++ ruby/lib/pattern_matching.rb
@@ -1,23 +1,71 @@
-class VariableMatcher
-  attr_accessor :variable
-  def call(algo)
+
+class Symbol
+  def call(something)
     true
   end
 end
+
+class PatternMatching
+
+  def val(unVal)
+    ValueMatcher.new(unVal)
+  end
+
+  def type(aType)
+    TypeMatcher.new(aType)
+  end
+
+  def list(aList, match_size = true)
+    ListMatcher.new(aList, match_size)
+  end
+
+end
+
+
 
 class ValueMatcher
 
   attr_accessor :valor
 
+  def initialize(aValue)
+    self.valor = aValue
+  end
+
   def call(value)
     value == valor
   end
+
 end
 
 class TypeMatcher
   attr_accessor :type
 
+  def initialize(aType)
+    self.type = aType
+  end
+
   def call(value)
-    value.class == type
+    value.is_a?(type)
+  end
+
+end
+
+class ListMatcher
+  attr_accessor :list, :match_size
+
+  def initialize(alist, match_size)
+
+    self.list = alist
+    self.match_size = match_size
+
+  end
+
+  def call(list_to_compare)
+
+    if match_size
+    list.all? {|e| list_to_compare.include?(e)}
+    else
+    list_to_compare.all? {|e| list.include?(e)}
+    end
   end
 end
--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,65 +1,72 @@
 require 'rspec'
 require_relative '../lib/pattern_matching'
 
-describe 'Pattern Matching Testing' do
+describe 'Matchers Básicos - Simbolo - Valor - Tipo - Lista' do
   it 'el call de cualquier Matcher de Variable da true' do
-    variableMatcher = VariableMatcher.new
-    variableMatcher.variable = :eLoco
 
-    expect(variableMatcher.call(:eLoco)).to eq(true)
+
+    expect(:algo.call('cualquierCosa')).to eq(true)
   end
 
   it 'matcher de valor 5 con call 5 es verdadero' do
 
-    matcherValor = ValueMatcher.new
-    matcherValor.valor = 5
-
-    expect(matcherValor.call(5)).to eq(true)
+    expect(PatternMatching.new.val(5).call(5)).to eq(true)
 
   end
 
   it 'matcher de valor 5 con call 4 es falso' do
 
-    matcherValor = ValueMatcher.new
-    matcherValor.valor = 4
 
-    expect(matcherValor.call(5)).to eq(false)
+    expect(PatternMatching.new.val(5).call(4)).to eq(false)
 
   end
 
   it 'matcher de valor 5 con call "5" es falso' do
 
-    matcherValor = ValueMatcher.new
-    matcherValor.valor = 5
-
-    expect(matcherValor.call("5")).to eq(false)
+    expect(PatternMatching.new.val(5).call("5")).to eq(false)
 
   end
 
   it 'matcher de valor "5" con call "5" es verdadero' do
 
-    matcherValor = ValueMatcher.new
-    matcherValor.valor = "5"
-
-    expect(matcherValor.call("5")).to eq(true)
+    expect(PatternMatching.new.val("5").call("5")).to eq(true)
 
   end
 
   it 'matcher de tipo Integer es verdadero para call 5' do
 
-    type = TypeMatcher.new
-    type.type = Integer
 
-    expect(type.call(5)).to eq(true)
+    expect(PatternMatching.new.type(Integer).call(5)).to eq(true)
 
   end
 
   it 'matcher de tipo Integer es falso para call "5"' do
 
-    type = TypeMatcher.new
-    type.type = Integer
+    expect(PatternMatching.new.type(Integer).call("5")).to eq(false)
 
-    expect(type.call("5")).to eq(false)
+  end
+
+  it 'matcher de lista con match_zise y con su misma lista' do
+
+    expect(PatternMatching.new.list([1, 2, 3, 4]).call([1,2,3,4])).to eq(true)
+
+  end
+
+  it 'matcher de lista con match_size y su misma lista menos un elemento' do
+
+    expect(PatternMatching.new.list([1,2,3,4]).call([1,2,3])).to eq(false)
+
+  end
+
+  it 'matcher de lista sin match_size y su misma lista menos un elemento' do
+
+    expect(PatternMatching.new.list([1,2,3,4], false).call([1,2,3])).to eq(true)
+
+  end
+
+  it 'Combinación de Matcher de Lista con Matcher de Simbolo' do
+
+    expect(PatternMatching.new.list([:uno,:dos,:tres,:cuatro]).call([1,2,3,4]))
 
   end
 
